b754e6680e66d653fc6eea179c81bf8d0d15c8f7
IvailoA05 <ivailooo55@abv.bg>
2024-01-27 19:40:16 +0200

    Added YouTube embed videos to the articles

--- BulgariaTimeLine/Views/BulgariaCreationPage.xaml.cs
+++ BulgariaTimeLine/Views/BulgariaCreationPage.xaml.cs
@@ -2,8 +2,12 @@ namespace BulgariaTimeLine;
 
 public partial class BulgariaCreationPage : ContentPage
 {
-	public BulgariaCreationPage()
+    public string YouTubeHtml { get; set; }
+    public BulgariaCreationPage()
 	{
 		InitializeComponent();
-	}
+
+        YouTubeHtml = "<html><body><iframe width='100%' height='200' src='https://www.youtube.com/embed/0EHzk2GHLrI' frameborder='0' allowfullscreen></iframe></body></html>";
+        BindingContext = this;
+    }
 }
