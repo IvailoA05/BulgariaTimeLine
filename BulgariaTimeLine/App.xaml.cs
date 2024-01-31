4a923b2e39792706dab40749c70c52d70a4115bf
IvailoA05 <ivailooo55@abv.bg>
2024-01-31 19:24:25 +0200

    Fixed the bug with the dark theme

--- BulgariaTimeLine/App.xaml.cs
+++ BulgariaTimeLine/App.xaml.cs
@@ -9,7 +9,8 @@ public partial class App : Application
 		InitializeComponent();
 
 		MainPage = new AppShell();
-	}
+        Application.Current.UserAppTheme = AppTheme.Light;
+    }
     static DatabaseHelper database;
 
     // Create the database connection as a singleton.
